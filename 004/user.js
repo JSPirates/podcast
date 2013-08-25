16de57ef3d20f15bec3c105ce95bf6de1b647e4d
Boris Egorov <dolphin278@gmail.com>
2013-08-25 17:30:00 +0400

    004 Api implementation

--- 004/user.js
+++ 004/user.js
@@ -1,29 +1,58 @@
 /*
+    Получаем идентификатор пользователя в нашей БД для
+    поиска. В реальности необходим свой собственный идентификатор.
+
+    Поля provider и id — свойства структуры профиля, которую возвращает
+    PasssportJS — http://passportjs.org/guide/profile/
+ */
+function prepareId(profile) {
+    return [profile.provider, profile.id].join(':');
+}
+
+/*
     Найти существующего, или создать запись пользователя
     по данным из PassportJS.
  */
 function findOrCreateUser(db, profile, callback) {
+    var id = prepareId(profile);
 
+    getUser(db, id, onFetchedUser);
+
+    function onFetchedUser(error, user) {
+        if (error) { return callback(error); }
+        if (user) {
+            // Нашли пользователя, можем возвращать
+            return callback(null, user);
+        } else {
+            // Пользователь не найден, необходимо создать новую запись и вернуть ее
+            return storeUser(db, profile, callback);
+        }
+    }
 }
 /*
     Вернуть учетную запись пользователя
  */
 function getUser(db, id, callback) {
-
+    console.log('load user with id', id);
+    db.get(id, callback);
 }
-
 /*
     Сохранить запись о пользователе в БД
  */
 function storeUser(db, user, callback) {
-
+    var id = prepareId(user);
+    console.log('storeUser with id', id);
+    db.put(id, user, function (error) {
+        if (error) { callback(error); }
+        else { callback(null, user); }
+    });
 }
 
 module.exports = function (db) {
-
-
-    api = {
-        findOrCreateUser: findOrCreateUser.bind(null, db)
+    return {
+        findOrCreateUser: findOrCreateUser.bind(null, db),
+        serializeUser: storeUser.bind(null, db),
+        deserializeUser: getUser.bind(null, db),
+        getUser: getUser.bind(null, db)
     }
-
 };
